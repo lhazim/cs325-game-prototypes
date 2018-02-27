425b2b1d839b8e06fc9ec66d1b901aa3dbd6caa7
layanne <layanne.hazim@gmail.com>
2018-02-27 14:22:43 -0500

    lives + game over

---
+++ da4/js/Game.js
@@ -0,0 +1,171 @@
+"use strict";
+
+GameStates.makeGame = function( game, shared ) {
+	var cursors = null;
+	var background = null;
+	var world = null; 
+	var ground = null;
+    // blue stars: 10pts, 1.25 speed of gold stars
+	// gold stars: 5 pts
+    var stars_blue = null;
+	var stars_gold = null;
+	var star = null; 
+	var fainting_couch = null;
+	// score value & scoreboard text
+	var score = 0; 
+	var scoreboard = null;
+	var lives = 5;
+	var life = null; 
+	//timer decreases as score gets higher 
+	//increasing frequency of stars increases difficulty
+	//speed of fainting couch must increase in proportion 
+	var blue_speed = null; 
+	var gold_speed = null;
+	var timer_blue = 8000; 
+	var timer_gold = 1000; 
+	//need to check current game time and in update loop check for elapsed time (timer_blue and timer_gold)
+	//if time has elapsed -> create new star
+	var time_check = null; 
+	var last_spawn_blue = 0; 
+	var last_spawn_gold = 0;
+	//num of stars generated on the board
+	var num_blue = 0; 
+	var num_gold = 0;
+
+	function caught(player, star){
+		score += star.value;
+		scoreboard.text = 'Score: ' + score; 
+		if(star.color == 'b'){
+			num_blue--;
+		}
+		if(star.color == 'g'){
+			num_gold--; 
+		}
+		star.kill();
+	}
+	
+	function death(ground, star){
+		if(star.color == 'b'){
+			num_blue--;
+		}
+		if(star.color == 'g'){
+			num_gold--; 
+		}
+		star.kill();
+		lives--;
+		life.text = 'Lives: ' + lives; 
+	}
+    
+    return {
+    
+        create: function () {
+            
+			var music = game.add.audio('gameMusic');
+			music.play(); 
+			
+			world = game.add.group();
+			
+			background = game.add.sprite(300, 300, 'background');
+			background.scale.setTo(0.5,0.5);
+			background.anchor.setTo(0.5,0.5);
+			world.add(background);
+			
+            // Display score in the top right
+			var style = { font: "32px Century Gothic", fill: "#fffeff" }; 
+			scoreboard = game.add.text(40,40,'Score: 0',style);
+			life = game.add.text(40,70,'Lives: 5', style); 
+			
+			// Create player
+			fainting_couch = game.add.sprite(game.world.centerX, 415, 'couch');
+			fainting_couch.scale.setTo(0.67,0.67);
+			fainting_couch.anchor.setTo( 0.5, 0.5 );
+			
+			// Enable physics
+			game.physics.arcade.enable(fainting_couch);
+			fainting_couch.body.collideWorldBounds = true; 
+			fainting_couch.body.immovable = true; 
+			fainting_couch.speed = 190; 
+			
+			cursors = game.input.keyboard.createCursorKeys();
+			
+			ground = game.add.sprite(0,590,'ground');
+			ground.anchor.setTo(0,0);
+			
+			game.physics.arcade.enable(ground); 
+			ground.body.immovable = true; 
+			ground.body.onCollide = new Phaser.Signal();
+			ground.body.onCollide.add(death, this);
+			
+			stars_blue = game.add.group(); 
+			stars_blue.enableBody = true; 
+			stars_gold = game.add.group();
+			stars_gold.enableBody = true; 
+			
+			star = stars_gold.create(game.world.randomX, -150, 'gold');
+			star.value = 5; 
+			
+			fainting_couch.body.onCollide = new Phaser.Signal();
+			fainting_couch.body.onCollide.add(caught, this);
+        },
+    
+        update: function () {
+    
+
+			
+			//  Reset the players velocity (movement)
+			fainting_couch.body.velocity.x = 0;
+			// Get current time
+			time_check = game.time.time;
+			// Check for collisions: couch and stars
+			// Check for collisions: ground and stars
+			game.physics.arcade.collide(fainting_couch, stars_blue);
+			game.physics.arcade.collide(fainting_couch, stars_gold); 
+			game.physics.arcade.collide(ground, stars_blue);
+			game.physics.arcade.collide(ground, stars_gold);
+			
+			if (lives == 0){
+				// reset lives
+				// goto: GameOver
+				lives = 5;
+				game.state.start('GameOver');
+			}
+			
+			if(time_check - last_spawn_blue > timer_blue && num_blue < 3){
+				timer_blue = game.rnd.normal()*8000; 
+				last_spawn_blue = time_check;
+				star = stars_blue.create(game.world.randomX, -300, 'blue');
+				star.scale.setTo(0.5,0.5);
+				star.body.velocity.y = 100 + Math.random() * 50;
+				star.value = 10;
+				star.color = 'b';
+				num_blue++;
+			}
+			
+			if(time_check - last_spawn_gold > timer_gold && num_gold < 4){
+				timer_gold = game.rnd.normal()*2000; 
+				last_spawn_gold = time_check;
+				star = stars_gold.create(game.world.randomX, -300, 'gold');
+				star.scale.setTo(0.5,0.5);
+				star.body.velocity.y = 80 + Math.random() * 50;
+				star.value = 5;
+				star.color = 'g';
+				num_gold++;
+			}
+
+			if (cursors.left.isDown)
+			{
+				//  Move to the left
+				fainting_couch.body.velocity.x = -1*fainting_couch.speed;
+			}
+			else if (cursors.right.isDown)
+			{
+				//  Move to the right
+				fainting_couch.body.velocity.x = fainting_couch.speed;
+			}
+			//else if(cursors.down.isDown)
+			//{
+			//	world.angle = 90;
+			//}
+        }
+    };
+};
---
+++ da4/js/Preloader.js
@@ -0,0 +1,48 @@
+"use strict";
+
+GameStates.makePreloader = function( game ) {
+
+	var background = null;
+	var preloadBar = null;
+	var playButton = null;
+
+	function startGame(pointer) {
+
+
+        //	And start the actual game
+        game.state.start('Game');
+
+    }
+	
+    return {
+    
+        preload: function () {
+
+            background = game.add.sprite(0, 0, 'background');
+			background.scale.setTo(0.5,0.5);
+    
+            //	Load the game assets
+            game.load.audio('gameMusic', ['assets/otts.flac']);
+            //	+ lots of other required assets here
+			game.load.image( 'blue','assets/star_blue.png');
+			game.load.image('gold','assets/star_gold.png');
+			game.load.image('couch','assets/couch.png');
+			game.load.image('ground','assets/ground.png');
+        },
+    
+        create: function () {
+    
+			var style = { font: "18px Century Gothic", fill: "#fffeff", align: "left", boundsAlignH: "left", boundsAlignV: "top", wordWrap: true, wordWrapWidth: 450 }; 
+			game.add.text(75,100,'“He stared up at the stars: and it seemed to him then that they were dancers, stately and graceful, performing a dance almost infinite in its complexity. He imagined he could see the very faces of the stars; pale, they were, and smiling gently, as if they had spent so much time above the world, watching the scrambling and the joy and the pain of the people below them, that they could not help being amused every time another little human believed itself the center of its world, as each of us does.” \n --- Neil Gaiman, Stardust ',style);
+			var style2 = { font: "52px Century Gothic", fill: "#fffeff" };
+			var start = game.add.text(300,500,'Start',style2); 
+			start.inputEnabled = true; 
+			start.events.onInputDown.add( startGame, this );
+        },
+    
+        update: function () {
+    
+        }
+    
+    };
+};
